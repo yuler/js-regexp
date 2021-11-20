7f377d5389507b7e404bdb563fc1de9ef7390706
yuler <joyful4yu@gmail.com>
2021-11-20 21:36:33 +0800

    feat: add regexp-rules

---
+++ packages/regexp-rules/index.js
@@ -0,0 +1,22 @@
+export const REGEXP_ID_NUMBER =
+	/^[1-9]\d{5}(?:18|19|20)\d{2}(?:0[1-9]|10|11|12)(?:0[1-9]|[1-2]\d|30|31)\d{3}[\dXx]$/
+
+export const REGEXP_EMAIL = /^([\w\-.+])+\@([\w\-.])+\.([A-Za-z]{2,})$/
+
+export const REGEXP_USERNAME = /^[\w-]{4,16}$/
+
+export const REGEXP_WECHAT_ID = /^[a-zA-Z][-\w]{5,19}$/
+
+export const REGEXP_COLOR_HEX = /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/
+
+export const REGEXP_CHINESE = /^[\u4e00-\u9fa5]{0,}$/
+
+export const REGEXP_CHINESE_NAME = /^(?:[\u4e00-\u9fa5·]{2,16})$/
+
+export const REGEXP_MOBILE_NUMBER = /^1[34578]\d{9}$/
+
+// TODO: HTML
+
+// TODO: JavaScript Code
+
+// TODO: IP Address
